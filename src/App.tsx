4340d0bc59a5831a6fa2bc5f7ad9ba98f901c195
austinpower1258 <austinwang115@gmail.com>
2023-11-30 12:25:56 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [data, setData] = useState([]);
+
+  const handleSearchChange = (event: any) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/search/?queryy=${encodeURIComponent(searchTerm)}`);
+      const data = await response.json();
+      console.log(data);
+      let finalData = shuffleArray(data);
+      setData(finalData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const shuffleArray = (array: any) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Spood</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          placeholder="Search..."
+          className="search-input"
+        />
+        <button onClick={handleSearch} className="search-button">
+          Search
+        </button>
+      </div>
+      <div className="image-grid">
+        {data.map((data, index) => (
+          <a href={data['source_url']}>
+            <div key={data['id']} className="grid-item">
+              <img src={data['image_url']} alt={`${index}-${data['id']}`} />
+            </div>
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Post.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './Post.css';
+
+const Post: React.FC = () => {
+  const [dishName, setDishName] = useState('');
+  const [rating, setRating] = useState(0);
+  const [file, setFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+
+  const handleDishNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDishName(event.target.value);
+  };
+
+  const handleRatingChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRating(Number(event.target.value));
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      setFile(file);
+      setPreviewUrl(URL.createObjectURL(file));
+    }
+  };
+
+  return (
+    <div className="post-container">
+      <h1>Post A Dish</h1>
+      <label className="post-label">
+        Dish Name:
+        <input type="text" className="post-input" value={dishName} onChange={handleDishNameChange} />
+      </label>
+      <label className="post-label">
+        Rating (0-100):
+        <input type="number" className="post-input" min="0" max="100" value={rating} onChange={handleRatingChange} />
+      </label>
+      <label className="post-label">
+        Upload Picture:
+        <input type="file" className="post-input" onChange={handleFileChange} accept="image/*" />
+      </label>
+      {previewUrl && (
+        <div className="image-preview">
+          <img src={previewUrl} alt="Preview" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Post;
